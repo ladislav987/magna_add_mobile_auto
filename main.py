be2d31fd54d03da2b830ef158a9ce6aa0fd2126f
ladichva <ladislavchvastas@gmail.com>
2023-02-22 14:53:44 +0100

    functional solution

--- main.py
+++ main.py
@@ -8,27 +8,245 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 import openpyxl
 
+import definition_file as definition
+import xpath_file as xpath
 
-def main():
+
+def main(mobil_name, sn_number, imei1_number, imei2_number, end_of_life_date_parameter, iteration):
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
 
+    # open url
+    definition.open_url(driver)
+
+    # click on add new mobile
+    definition.function(driver,
+                        'xpath',
+                        xpath.add_mobile,
+                        'click',
+                        '     Click on add new mobile....................OK',
+                        'Please help me and click on add new mobile button, then press Enter. 🤗')
+
+    # write mobile name
+    definition.function(driver,
+                        'xpath',
+                        xpath.name,
+                        'send_keys',
+                        '     Write mobile name..........................OK',
+                        'Please help me and write mobile name, then press Enter. 🤗',
+                        mobil_name)
+
+    # write SN
+    definition.function(driver,
+                        'xpath',
+                        xpath.sn,
+                        'send_keys',
+                        '     Write mobile SN............................OK',
+                        'Please help me and write mobile SN, then press Enter. 🤗',
+                        sn_number)
+
+
+    # write asset manager
+    definition.function_with_wait(
+                        driver,
+                        'xpath',
+                        xpath.input_assets_manager,
+                        xpath.input_assets_manager_accept,
+                        'send_keys',
+                        '     Write asset manager........................OK',
+                        'Please help me and write asset manager, then press Enter. 🤗',
+                        'Zolna, Matus | 2394-Active (Matus.Zolna@magna.com)')
+
+# write asset owner
+    definition.function_with_wait(
+                        driver,
+                        'xpath',
+                        xpath.input_assets_owner,
+                        xpath.input_assets_owner_accept,
+                        'send_keys',
+                        '     Write asset owner..........................OK',
+                        'Please help me and write asset owner, then press Enter. 🤗',
+                        'Chovanec, Dominik | 2394-Active (Dominik.Chovanec@magna.com)')
+
+# write stock keeping unit
+    definition.function_with_wait(
+                        driver,
+                        'xpath',
+                        xpath.input_stock_keeping_unit,
+                        xpath.input_stock_keeping_unit_accept,
+                        'send_keys',
+                        '     Write stock keeping unit...................OK',
+                        'Please help me and write stock keeping unit, then press Enter. 🤗',
+                        'SKU25606 - Smart Phone - Apple, iPhone SE 3rd Gen')
+
+    # select computer role
+    definition.function(driver,
+                        'select',
+                        xpath.computer_role,
+                        'select_by_index',
+                        '     Pick computer role.........................OK',
+                        'Please help me and pick computer role, then press Enter. 🤗',
+                        '',
+                        2)
+
+    # click on save button
+    definition.function(driver,
+                        'xpath',
+                        xpath.save_button,
+                        'click',
+                        '     Click on save button.......................OK',
+                        'Please help me and click on save button, then press Enter. 🤗')
+
+    # click on edit button
+    definition.function(driver,
+                        'xpath',
+                        xpath.edit_button,
+                        'click',
+                        '     Click on edit button.......................OK',
+                        'Please help me and click on edit button, then press Enter. 🤗')
+
+    # click on do not inherit check box
+    definition.function(driver,
+                        'xpath',
+                        xpath.do_not_inherit_check_box,
+                        'click',
+                        '     Click on do not inherit check box..........OK',
+                        'Please help me and click on do not inherit check box, then press Enter. 🤗')
+
+    # write inventory name
+    definition.function(driver,
+                        'xpath',
+                        xpath.inventory_name,
+                        'send_keys',
+                        '     Write inventory number.....................OK',
+                        'Please help me and write inventory number, then press Enter. 🤗',
+                        mobil_name)
+# write imei1
+    definition.function(driver,
+                        'xpath',
+                        xpath.imei1,
+                        'send_keys',
+                        '     Write imei1 number.........................OK',
+                        'Please help me and write imei1 number, then press Enter. 🤗',
+                        imei1)
+# write imei2
+    definition.function(driver,
+                        'xpath',
+                        xpath.imei2,
+                        'send_keys',
+                        '     Write imei2 number.........................OK',
+                        'Please help me and write imei2 number, then press Enter. 🤗',
+                        mobil_name)
+
+    # click on do not inherit check box
+    definition.function(driver,
+                        'xpath',
+                        xpath.end_of_life_button,
+                        'click',
+                        '     Click on end of life button................OK',
+                        'Please help me and click on end of life button, then press Enter. 🤗')
+
+    # write end of life date
+    definition.function(driver,
+                        'xpath',
+                        xpath.end_of_life_input,
+                        'send_keys',
+                        '     Write end of life date.....................OK',
+                        'Please help me and write inventory number, then press Enter. 🤗',
+                        end_of_life_date_parameter)
+
+    # write 60 days reminder
+    definition.function(driver,
+                        'xpath',
+                        xpath.sixty_days_input,
+                        'send_keys',
+                        '     Write 60 days reminder.....................OK',
+                        'Please help me and write 60 days reminder, then press Enter. 🤗',
+                        '60')
+
+# write mek level 1 role
+    definition.function(driver,
+                        'xpath',
+                        xpath.input_task_recipient_role,
+                        'send_keys',
+                        '     Write MEK level 1 role.....................OK',
+                        'Please help me and write MEK level 1 role, then press Enter. 🤗',
+                        'MEG_MEK_ServiceDesk_Level_1')
+
+    # click on order
+    definition.function(driver,
+                        'xpath',
+                        xpath.order_button,
+                        'click',
+                        '     Click on order button......................OK',
+                        'Please help me and click on order button, then press Enter. 🤗')
+
+    # select proprietary
+    definition.function(driver,
+                        'select',
+                        xpath.pick_proprietary,
+                        'select_by_index',
+                        '     Pick proprietary...........................OK',
+                        'Please help me and pick proprietary, then press Enter. 🤗',
+                        '',
+                        1)
+
+    # click on done
+    definition.function(driver,
+                        'xpath',
+                        xpath.done_button,
+                        'click',
+                        '     Click on done button.......................OK',
+                        'Please help me and click on done button, then press Enter. 🤗')
+
+    time.sleep(1)
+
+    # click on more
+    definition.function(driver,
+                        'xpath',
+                        xpath.more_button,
+                        'click',
+                        '     Click on more button.......................OK',
+                        'Please help me and click on more button, then press Enter. 🤗')
+
+    time.sleep(1)
+
+    # click on new window
+    definition.function(driver,
+                        'xpath',
+                        xpath.new_window_button,
+                        'click',
+                        '     Click on new window button.................OK',
+                        'Please help me and click on new window button, then press Enter. 🤗')
+
+    new_window = driver.window_handles[1]
+    driver.switch_to.window(new_window)
+
+    time.sleep(4)
+
+    definition.write_to_excel(driver.current_url, mobil_name, iteration)
+
 
 # load excel with its path
-path = r'C:\Users\ladichva\PycharmProjects\magna_add_ntb_auto\inputs.xlsx'
+path = r'inputs.xlsx'
 wrkbk = openpyxl.load_workbook(path)
 
 sh = wrkbk.active
 
-# iterate through excel and display data
-for i in range(1, sh.max_row + 1):
-    mek_name = sh.cell(row=i, column=1)
-    ntb_service = sh.cell(row=i, column=2)
-    end_of_life_date = sh.cell(row=1, column=3)
 
-    print(mek_name.value)
-    print(ntb_service.value)
+# iterate through excel and display data
+for i in range(2, sh.max_row + 1):
+    mobile_name = sh.cell(row=i, column=1)
+    sn_number = sh.cell(row=i, column=2)
+    imei1 = sh.cell(row=i, column=3)
+    imei2 = sh.cell(row=i, column=4)
+    end_of_life = sh.cell(row=2, column=5)
+
+    print(mobile_name.value)
+    print(sn_number.value)
+    print(imei1.value)
+    print(imei2.value)
     print(" ")
-    main(mek_name.value, ntb_service.value, end_of_life_date.value, i)
+    main(mobile_name.value, sn_number.value, imei1.value, imei2.value, end_of_life.value, i)
 
 print('Process ended successfully 😊😎🤯')
--- xpath_file.py
+++ xpath_file.py
@@ -1,2 +1,43 @@
 
 url = 'https://itserviceportal.magna.global/wm/app-Assets/search-page/5e3e848c-d8fd-e611-e8ab-8c89a5649695/nofilter/'
+
+add_mobile = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/mx-common-actions/mx-horizontal-action-list/div/div[1]/button'
+
+name = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[3]/div[1]/div/div/mx-text-box/md-input-container/input'
+sn = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[4]/div[1]/div/div/mx-text-box/md-input-container/input'
+
+input_assets_manager = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[5]/div[1]/div/div/mx-object-picker/mx-picker/div/md-autocomplete/md-autocomplete-wrap/md-input-container/input'
+input_assets_manager_accept = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[5]/div[1]/div/div/mx-object-picker/mx-picker/div/md-autocomplete/md-autocomplete-wrap/md-input-container/div[2]/div[2]'
+
+input_assets_owner = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[5]/div[2]/div/div/mx-object-picker/mx-picker/div/md-autocomplete/md-autocomplete-wrap/md-input-container/input'
+input_assets_owner_accept = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[5]/div[2]/div/div/mx-object-picker/mx-picker/div/md-autocomplete/md-autocomplete-wrap/md-input-container/div[2]/div[2]'
+
+input_stock_keeping_unit = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[6]/div[1]/div/div/mx-object-picker/mx-picker/div/md-autocomplete/md-autocomplete-wrap/md-input-container/input'
+input_stock_keeping_unit_accept = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[6]/div[1]/div/div/mx-object-picker/mx-picker/div/md-autocomplete/md-autocomplete-wrap/md-input-container/div[2]/div[2]'
+
+computer_role = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[1]/div/div/div/div/div/div[8]/div[1]/div/div/mx-enum-picker/mx-select/md-input-container/select'
+
+save_button = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-wizard-view/div/mx-wizard/div/ng-form/div/div[3]/div[2]/mx-button[2]/button'
+
+edit_button = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[1]/mx-horizontal-action-list/div/div[1]/button[1]'
+
+do_not_inherit_check_box = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[1]/div/div/div/div/div[4]/mx-checkbox/md-input-container/md-checkbox/div[2]/span'
+
+inventory_name = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[2]/div[1]/div/div/div/div[2]/mx-text-box[1]/md-input-container/input'
+
+imei1 = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[2]/div[1]/div/div/div/div[1]/mx-text-box[2]/md-input-container'
+imei2 = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[2]/div[1]/div/div/div/div[2]/mx-text-box[4]/md-input-container'
+
+end_of_life_button = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[5]/div/div/div/mx-tabs/md-tabs/md-tabs-wrapper/md-tabs-canvas/md-pagination-wrapper/md-tab-item[8]'
+end_of_life_input = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[5]/div/div/div/mx-tabs/md-tabs/md-tabs-content-wrapper/md-tab-content[8]/div/div/div/div/div[2]/div/div/mx-date-time-picker/div/md-input-container/md-datepicker/div[1]/input'
+sixty_days_input = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[5]/div/div/div/mx-tabs/md-tabs/md-tabs-content-wrapper/md-tab-content[8]/div/div/div/div/div[3]/div/div/mx-numeric-edit/md-input-container/input'
+input_task_recipient_role = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[5]/div/div/div/mx-tabs/md-tabs/md-tabs-content-wrapper/md-tab-content[8]/div/div/div/div/div[4]/div/div/mx-object-picker/mx-picker/div/md-autocomplete/md-autocomplete-wrap/md-input-container/input'
+
+order_button = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[5]/div/div/div/mx-tabs/md-tabs/md-tabs-wrapper/md-tabs-canvas/md-pagination-wrapper/md-tab-item[4]'
+pick_proprietary = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[2]/mx-common-dialog-content/ng-form/mx-dialog-container/div[1]/div/div/mx-content-widget-view/mx-content-widget-view-dialog-page/div/div/div/div[5]/div/div/div/mx-tabs/md-tabs/md-tabs-content-wrapper/md-tab-content[4]/div/div/div/div/div/div/div/div/div[4]/mx-enum-picker/mx-select/md-input-container/select'
+
+done_button = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-body/div/div/mx-dialog-view/div/div[3]/button[3]'
+
+more_button = '/html/body/div[1]/mx-app/div/mx-shell/div/div/div/div/div[2]/mx-side-panel/div/div[1]/div/mx-side-panel-tab-header/div/div[2]/mx-side-panel-ui-actions/md-menu-bar/md-menu/button/md-icon'
+
+new_window_button = '/html/body/div[5]/md-menu-content/md-menu-item[1]/button'
